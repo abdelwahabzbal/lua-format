e3817b343aceefadc34deff728011fd4441b8667
awh6al <awh6al@yandex.com>
2020-10-16 20:59:58 +0100

    generate lua parser in the src directory

--- xmake.lua
+++ xmake.lua
@@ -4,13 +4,12 @@ add_repositories("yaml https://github.com/jbeder/yaml-cpp.git")
 add_repositories("catch https://github.com/catchorg/Catch2.git")
 
 local repo = "$(configdir)/repositories/"
-local buildir = "$(buildir)/" -- FIX: find better place for parser 
 
 before_build(function()
-    if not os.exists(buildir .. "parser") then
+    if not os.exists("src/parser") then
         os.execv("antlr4", {
-            "-Dlanguage=Cpp", "-no-listener", "-visitor", "-o",
-            buildir .. "parser", "Lua.g4"
+            "-Dlanguage=Cpp", "-no-listener", "-visitor", "-o", "src/parser",
+            "Lua.g4"
         })
     end
 end)
@@ -18,18 +17,17 @@ end)
 target("lua-format")
 set_kind("binary")
 add_cxxflags("-std=c++17", "-fuse-ld=lld", {force = true})
-add_files("src/*.cpp", repo .. "antlr/runtime/Cpp/runtime/src/**.cpp",
-          repo .. "yaml/src/**.cpp", "build/parser/*.cpp")
-add_includedirs("src", "build/parser", repo .. "args", repo .. "yaml/include",
+add_files("src/**.cpp", repo .. "antlr/runtime/Cpp/runtime/src/**.cpp",
+          repo .. "yaml/src/**.cpp")
+add_includedirs("src", "src/parser", repo .. "args", repo .. "yaml/include",
                 repo .. "antlr/runtime/Cpp/runtime/src")
 
 target("test")
 set_kind("binary")
 add_cxxflags("-std=c++17", "-fuse-ld=lld", "-fpermissive", {force = true})
 add_files("test/**.cpp", "src/**.cpp", repo .. "yaml/src/**.cpp",
-          repo .. "antlr/runtime/Cpp/runtime/src/**.cpp",
-          "$(buildir)/parser/*.cpp")
+          repo .. "antlr/runtime/Cpp/runtime/src/**.cpp")
 del_files("src/main.cpp")
-add_includedirs("$(buildir)/parser", repo .. "yaml/include",
+add_includedirs("src", "src/parser", repo .. "yaml/include",
                 repo .. "antlr/runtime/Cpp/runtime/src", "src", repo .. "args",
                 repo .. "catch/include")
