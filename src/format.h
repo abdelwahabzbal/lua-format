ed45bf44ebc648f2df52e0f005edfe1780de1601
awh6al <awh6al@yandex.com>
2020-10-25 18:38:06 +0100

    check for c++17 filesystem header

--- src/format.h
+++ src/format.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <filesystem>
 #include <iostream>
 #include <string>
 
@@ -8,7 +7,7 @@
 
 using namespace std;
 
-#define PROJECT_PATH string("/home/awh6al/code/cpp/lua-format")
+#define PROJECT_PATH string("lua-format")
 
 string lua_format(const string& input, const Config& config);
 string lua_format(istream& stream, const Config& config);
